49305915bca84fe62379226dd21e754ea58504cb
Osias Cesar <osiasnt@gmail.com>
2019-07-28 01:41:52 -0300

    bandeira da casa com session
    aula 99

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,5 +1,5 @@
 module.exports.load = function(application, req, res){
-    if(req.session.autorizado) res.render('jogo');
+    if(req.session.autorizado) res.render('jogo',{img_casa:req.session.casa});
     else res.send("necessario fazer login");
 };
 
